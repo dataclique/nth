6ccf365c637d0facfeaf6482bb3fdcbfaa50c5a9
levsgit <shatorv.lev@gmail.com>
2025-03-10 17:55:06 +0400

    Feat: set-up first plot

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,11 @@ import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit"
 import { isValidSuiObjectId } from "@mysten/sui/utils"
 import { Box, Container, Flex, Heading } from "@radix-ui/themes"
 import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import { Counter } from "./Counter"
 import { CreateCounter } from "./CreateCounter"
 import { BackendStatus } from "./BackendStatus"
+import Plot from "./Plot"
 
 function App() {
   const currentAccount = useCurrentAccount()
@@ -14,50 +16,63 @@ function App() {
   })
 
   return (
-    <>
-      <Flex
-        position="sticky"
-        px="4"
-        py="2"
-        justify="between"
-        style={{
-          borderBottom: "1px solid var(--gray-a2)",
-        }}
-      >
-        <Box>
-          <Heading>dApp Starter Template</Heading>
-        </Box>
-
-        <Box>
-          <ConnectButton />
-        </Box>
-      </Flex>
-      <Container>
-        <BackendStatus />
-
-        <Container
-          mt="5"
-          pt="2"
+    <Router>
+      <>
+        <Flex
+          position="sticky"
           px="4"
-          style={{ background: "var(--gray-a2)", minHeight: 500 }}
+          py="2"
+          justify="between"
+          style={{
+            borderBottom: "1px solid var(--gray-a2)",
+          }}
         >
-          {currentAccount ? (
-            counterId ? (
-              <Counter id={counterId} />
-            ) : (
-              <CreateCounter
-                onCreated={id => {
-                  window.location.hash = id
-                  setCounter(id)
-                }}
-              />
-            )
-          ) : (
-            <Heading>Please connect your wallet</Heading>
-          )}
+          <Box>
+            <Heading>dApp Starter Template</Heading>
+            <Link to="/">Home</Link> | <Link to="/plot">Plot</Link>
+          </Box>
+
+          <Box>
+            <ConnectButton />
+          </Box>
+        </Flex>
+
+        <Container>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <BackendStatus />
+                  <Container
+                    mt="5"
+                    pt="2"
+                    px="4"
+                    style={{ background: "var(--gray-a2)", minHeight: 500 }}
+                  >
+                    {currentAccount ? (
+                      counterId ? (
+                        <Counter id={counterId} />
+                      ) : (
+                        <CreateCounter
+                          onCreated={id => {
+                            window.location.hash = id
+                            setCounter(id)
+                          }}
+                        />
+                      )
+                    ) : (
+                      <Heading>Please connect your wallet</Heading>
+                    )}
+                  </Container>
+                </>
+              }
+            />
+            <Route path="/plot" element={<Plot />} />
+          </Routes>
         </Container>
-      </Container>
-    </>
+      </>
+    </Router>
   )
 }
 
